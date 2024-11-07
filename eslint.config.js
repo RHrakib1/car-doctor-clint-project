eea38c0fe30ddd5de5de50e91d4fb757af828690
RHrakib1 <rakibhasanmd457@gmail.com>
2024-11-07 10:33:51 +0600

    add some one done

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+module.exports={
+  env:{node:true}
+}
 
 export default [
   { ignores: ['dist'] },
